661b75299d644a36fdfde2f324a66ad506681873
ubiluke <lucas.ciba@ubisoft.com>
2026-04-07 11:42:06 +0200

    Chomper and UI edits

--- Assets/3DGamekit/Scripts/Game/Audio/UISoundController.cs
+++ Assets/3DGamekit/Scripts/Game/Audio/UISoundController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class UISoundController : MonoBehaviour, IPointerEnterHandler
+public class UISoundController : MonoBehaviour
 {
     public AK.Wwise.Event UI_Back;
     public AK.Wwise.Event UI_Cancel;
@@ -13,57 +13,48 @@ public class UISoundController : MonoBehaviour, IPointerEnterHandler
     public AK.Wwise.Event UI_Text_Pop;
     public AK.Wwise.Event UI_Text_PopOut;
 
-    public void OnPointerEnter(PointerEventData eventData)
+    public void PlayHover()
     {
-        if (UI_Hover != null)
-            UI_Hover.Post(gameObject);
+        UI_Hover.Post(gameObject);
     }
 
     public void PlayClick()
     {
-        if (UI_Click != null)
-            UI_Click.Post(gameObject);
+        UI_Click.Post(gameObject);
     }
 
     public void PlayBack()
     {
-        if (UI_Back != null)
-            UI_Back.Post(gameObject);
+        UI_Back.Post(gameObject);
     }
 
     public void PlayCancel()
     {
-        if (UI_Cancel != null)
-            UI_Cancel.Post(gameObject);
+        UI_Cancel.Post(gameObject);
     }
 
     public void PlayESC()
     {
-        if (UI_ESC != null)
-            UI_ESC.Post(gameObject);
+        UI_ESC.Post(gameObject);
     }
 
     public void PlaySelect()
     {
-        if (UI_Select != null)
-            UI_Select.Post(gameObject);
+        UI_Select.Post(gameObject);
     }
 
     public void PlayStart()
     {
-        if (UI_Start != null)
-            UI_Start.Post(gameObject);
+        UI_Start.Post(gameObject);
     }
 
     public void PlayTextPop()
     {
-        if (UI_Text_Pop != null)
-            UI_Text_Pop.Post(gameObject);
+        UI_Text_Pop.Post(gameObject);
     }
 
     public void PlayTextPopOut()
     {
-        if (UI_Text_PopOut != null)
-            UI_Text_PopOut.Post(gameObject);
+        UI_Text_PopOut.Post(gameObject);
     }
 }
